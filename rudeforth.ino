a7f3ea508a4ea7aa80bdee64de7e154588fe6ba8
Andrew Fuller <qartis@gmail.com>
2023-01-25 22:16:32 -0800

    misc changes
    - remove some more stuff
    - add some features to 'see'
    - upgrade looping from more recent esp32forth implement forth versions of ":", ";", "create" and "does>" (create/does> are stolen from planckforth)

--- rudeforth.ino
+++ rudeforth.ino
@@ -6,29 +6,36 @@ typedef intptr_t cell_t;
 typedef int64_t dcell_t;
 typedef uint64_t udcell_t;
 
-
 #define HEAP_SIZE (100 * 1024)
 #define STACK_SIZE 512
 #define FLAG_IMMEDIATE 1
 
 #define DUP *++sp = tos
 #define DROP tos = *sp--
-#define COMMA(n) *g_sys.here++ = (n)
+#define COMMA(n) *g_sys.here++ = ((cell_t)n)
 #define IMMEDIATE() g_sys.latest[-1] |= FLAG_IMMEDIATE
-#define DOES(ip) *g_sys.latest = (cell_t) && OP_DODOES; g_sys.latest[1] = (cell_t) ip
-#define UMSMOD ud = *(udcell_t *) &sp[-1]; \
-               --sp; *sp = (cell_t) (ud % tos); \
-               tos = (cell_t) (ud / tos)
-#define SSMOD d = (dcell_t) *sp * (dcell_t) sp[-1]; \
-              --sp; *sp = (cell_t) (((udcell_t) d) % tos); \
-              tos = (cell_t) (d < 0 ? ~(~d / tos) : d / tos)
+#define UMSMOD ud = *(udcell_t *)&sp[-1]; \
+               --sp; *sp = (cell_t)(ud % tos); \
+               tos = (cell_t)(ud / tos)
+#define SSMOD d = (dcell_t)*sp * (dcell_t)sp[-1]; \
+              --sp; *sp = (cell_t) (((udcell_t)d) % tos); \
+              tos = (cell_t)(d < 0 ? ~(~d / tos) : d / tos)
+
+#define NEXT w = *ip++; goto **(void **)w
+#define CELL_LEN(n) (((n) + sizeof(cell_t) - 1) / sizeof(cell_t))
+#define FIND(name) find(name, sizeof(name) - 1)
+#define LOWER(ch) ((ch) & 0x5F)
+
+
 
 #define OPCODE_LIST \
+  X("'DOCOL", TICKDOCOL, DUP; tos = (cell_t)&&OP_DOCOLON) \
   X("0=", ZEQUAL, tos = !tos ? -1 : 0) \
   X("0<", ZLESS, tos = tos < 0 ? -1 : 0) \
   X("+", PLUS, tos += *sp; --sp) \
   X("UM/MOD", UMSMOD, UMSMOD) \
   X("*/MOD", SSMOD, SSMOD) \
+  X("*", MUL, tos *= *sp; --sp) \
   X("AND", AND, tos &= *sp; --sp) \
   X("OR", OR, tos |= *sp; --sp) \
   X("XOR", XOR, tos ^= *sp; --sp) \
@@ -37,18 +44,16 @@ typedef uint64_t udcell_t;
   X("OVER", OVER, DUP; tos = sp[-1]) \
   X("DROP", DROP, DROP) \
   X("@", AT, tos = *(cell_t *)tos) \
-  X("L@", LAT, tos = *(int32_t *)tos) \
   X("C@", CAT, tos = *(uint8_t *)tos) \
   X("!", STORE, *(cell_t *)tos = *sp; --sp; DROP) \
-  X("L!", LSTORE, *(int32_t *)tos = *sp; --sp; DROP) \
   X("C!", CSTORE, *(uint8_t *)tos = *sp; --sp; DROP) \
-  X("FILL", FILL, memset((void *)sp[-1], tos, *sp); sp -= 2; DROP) \
-  X("MOVE", MOVE, memmove((void *)sp[-1], (void *)*sp, tos); sp -= 2; DROP) \
   X("SP@", SPAT, DUP; tos = (cell_t) sp) \
   X("SP!", SPSTORE, sp = (cell_t *) tos; DROP) \
   X("RP@", RPAT, DUP; tos = (cell_t) rp) \
   X("RP!", RPSTORE, rp = (cell_t *) tos; DROP) \
   X(">R", TOR, ++rp; *rp = tos; DROP) \
+  X(",", COMMA_TOKEN, COMMA(tos); DROP) \
+  X("'", TICK, DUP; tos = parse(' ', (cell_t *)sp); tos = find((const char *)*sp, tos)) \
   X("R>", FROMR, DUP; tos = *rp; --rp) \
   X("R@", RAT, DUP; tos = *rp) \
   X("EXECUTE", EXECUTE, w = tos; DROP; goto **(void **)w) \
@@ -61,18 +66,12 @@ typedef uint64_t udcell_t;
   X("CELL", CELL, DUP; tos = sizeof(cell_t)) \
   X("FIND", FIND, tos = find((const char *)*sp, tos); --sp) \
   X("PARSE", PARSE, DUP; tos = parse(tos, (cell_t *)sp)) \
-  X("S>NUMBER?", CONVERT, \
-      tos = convert((const char *)*sp, tos, (cell_t *)sp); \
-      if (!tos) --sp) \
-  X("CREATE", CREATE, DUP; DUP; tos = parse(32, (cell_t *)sp); \
-                      create((const char *)*sp, tos, && OP_DOCREATE); \
-                         COMMA(0); --sp; DROP) \
-  X("DOES>", DOES, DOES((cell_t *)ip); ip = (cell_t *)*rp; --rp) \
+  X("S>NUMBER?", CONVERT, tos = convert((const char *)*sp, tos, (cell_t *)sp); if (!tos) --sp) \
+  X("HEADER", HEADER, DUP; DUP; tos = parse(' ', (cell_t *)sp); \
+                      create((const char *)*sp, tos, &&OP_DOCOLON); \
+                         --sp; DROP) \
   X("IMMEDIATE", IMMEDIATE, IMMEDIATE()) \
   X("'SYS", SYS, DUP; tos = (cell_t)&g_sys) \
-  X(":", COLON, DUP; DUP; tos = parse(32, (cell_t *)sp); \
-                create((const char *)*sp, tos, && OP_DOCOLON); \
-                   g_sys.state = -1; --sp; DROP) \
   X("EVALUATE1", EVALUATE1, \
       DUP; sp = (cell_t *)evaluate1((cell_t *)sp); \
       w = *sp; --sp; DROP; \
@@ -83,19 +82,13 @@ typedef uint64_t udcell_t;
   X("type", TYPE, Serial.write((const uint8_t *) *sp, tos); --sp; DROP) \
   X("ms", MS, delay(tos); DROP) \
   X("bye", BYE, exit(tos)) \
-  X(";", SEMICOLON, COMMA(g_sys.DOEXIT_XT); g_sys.state = 0) /* SEMICOLON MUST BE LAST */ \
-
-#define NEXT w = *ip++; goto **(void **) w
-#define CELL_LEN(n) (((n) + sizeof(cell_t) - 1) / sizeof(cell_t))
-#define FIND(name) find(name, sizeof(name) - 1)
-#define LOWER(ch) ((ch) & 0x5F)
-
+  X("DD", DD, for(cell_t *start = here + STACK_SIZE + STACK_SIZE; start < g_sys.here; start++) { printf("%08x: %08x\n", start, *start); } ) \
 
 static struct {
   const char *tib;
   cell_t ntib, tin, state, base;
-  cell_t *here, *latest, notfound;
-  cell_t DOLIT_XT, DOEXIT_XT;
+  cell_t *here, *latest;
+  cell_t NOTFOUND_XT, DOLIT_XT;
 } g_sys;
 
 static cell_t convert(const char *pos, cell_t n, cell_t *ret) {
@@ -151,13 +144,14 @@ static cell_t find(const char *name, cell_t len) {
 */
 
 static void create(const char *name, cell_t length, void *op) {
+  printf("create(%.*s)\n", length, name);
   memcpy(g_sys.here, name, length);  // name
   g_sys.here += CELL_LEN(length);
-  *g_sys.here++ = length;  // length
-  *g_sys.here++ = (cell_t) g_sys.latest;  // link
-  *g_sys.here++ = 0;  // flags
+  COMMA(length);  // length
+  COMMA(g_sys.latest);  // link
+  COMMA(0);  // flags
   g_sys.latest = g_sys.here;
-  *g_sys.here++ = (cell_t) op;  // code
+  COMMA(op);  // code
 }
 
 static char spacefilter(char ch) {
@@ -179,10 +173,10 @@ static cell_t *evaluate1(cell_t *sp) {
   cell_t call = 0;
   cell_t name;
   cell_t len = parse(' ', &name);
-  cell_t xt = find((const char *) name, len);
+  cell_t xt = find((const char *)name, len);
   if (xt) {
-    if (g_sys.state && !(((cell_t *) xt)[-1] & FLAG_IMMEDIATE)) {
-      *g_sys.here++ = xt;
+    if (g_sys.state && !(((cell_t *)xt)[-1] & FLAG_IMMEDIATE)) {
+      COMMA(xt);
     } else {
       call = xt;
     }
@@ -191,8 +185,8 @@ static cell_t *evaluate1(cell_t *sp) {
     cell_t ok = convert((const char *) name, len, &n);
     if (ok) {
       if (g_sys.state) {
-        *g_sys.here++ = g_sys.DOLIT_XT;
-        *g_sys.here++ = n;
+        COMMA(g_sys.DOLIT_XT);
+        COMMA(n);
       } else {
         *++sp = n;
       }
@@ -200,7 +194,7 @@ static cell_t *evaluate1(cell_t *sp) {
       *++sp = name;
       *++sp = len;
       *++sp = -1;
-      call = g_sys.notfound;
+      call = g_sys.NOTFOUND_XT;
     }
   }
   *++sp = call;
@@ -208,8 +202,7 @@ static cell_t *evaluate1(cell_t *sp) {
 }
 
 static void ueforth(void *here, const char *src, cell_t src_len) {
-  memset(&g_sys, 0, sizeof(g_sys));
-  g_sys.here = (cell_t *) here;
+  g_sys.here = (cell_t *)here;
   register cell_t *sp = g_sys.here; g_sys.here += STACK_SIZE;
   register cell_t *rp = g_sys.here; g_sys.here += STACK_SIZE;
   register cell_t tos = 0, *ip, w;
@@ -220,19 +213,22 @@ static void ueforth(void *here, const char *src, cell_t src_len) {
   OPCODE_LIST
 #undef X
 
-  g_sys.latest[-1] = FLAG_IMMEDIATE;  // Make ; IMMEDIATE
+  IMMEDIATE(); // Make ; IMMEDIATE
+
   g_sys.DOLIT_XT = FIND("DOLIT");
-  g_sys.DOEXIT_XT = FIND("EXIT");
-  g_sys.notfound = FIND("DROP");
-  ip = g_sys.here;
-  *g_sys.here++ = FIND("EVALUATE1");
-  *g_sys.here++ = FIND("BRANCH");
-  *g_sys.here++ = (cell_t) ip;
+  g_sys.NOTFOUND_XT = FIND("DROP");
   g_sys.base = 10;
+  g_sys.state = 0;
+  g_sys.tin = 0;
   g_sys.tib = src;
   g_sys.ntib = src_len;
 
-  /* start interpreting EVALUATE1/BRANCH/ip loop defined above */
+  ip = g_sys.here;
+  COMMA(FIND("EVALUATE1"));
+  COMMA(FIND("BRANCH"));
+  COMMA(ip);
+
+  /* start interpreting EVALUATE1/BRANCH loop defined above */
   NEXT;
 
 
@@ -241,19 +237,21 @@ static void ueforth(void *here, const char *src, cell_t src_len) {
 #undef X
 
   OP_DOCOLON: ++rp; *rp = (cell_t) ip; ip = (cell_t *) (w + sizeof(cell_t)); NEXT;
-  OP_DOCREATE: DUP; tos = w + sizeof(cell_t) * 2; NEXT;
-  OP_DODOES: DUP; tos = w + sizeof(cell_t) * 2;
-             ++rp; *rp = (cell_t) ip; ip = (cell_t *) *(cell_t *) (w + sizeof(cell_t)); NEXT;
 }
 
 const char boot[] =
+" HEADER : ' HEADER , -1 ALITERAL 'sys 3 cell * + ALITERAL ' ! , ' exit , "
+" HEADER ; ' exit ALITERAL ' , , 0 ALITERAL 'sys 3 cell * + ALITERAL ' ! , ' exit , IMMEDIATE "
+
+" : bl 32 ; "
+" : nl 10 ; "
 " : (   41 parse drop drop ; immediate "
+" : \\   nl parse drop drop ; immediate "
 " : 2drop ( n n -- ) drop drop ; "
 " : 2dup ( a b -- a b a b ) over over ; "
 " : nip ( a b -- b ) swap drop ; "
 " : rdrop ( r: n n -- ) r> r> drop >r ; "
 " : */ ( n n n -- n ) */mod nip ; "
-" : * ( n n -- n ) 1 */ ; "
 " : /mod ( n n -- n n ) 1 swap */mod ; "
 " : / ( n n -- n ) /mod nip ; "
 " : mod ( n n -- n ) /mod drop ; "
@@ -266,8 +264,6 @@ const char boot[] =
 " : > ( a b -- a>b ) swap - 0< ; "
 " : = ( a b -- a!=b ) - 0= ; "
 " : <> ( a b -- a!=b ) = 0= ; "
-" : bl 32 ; "
-" : nl 10 ; "
 " : 1+ 1 + ; "
 " : 1- 1 - ; "
 " : +! ( n a -- ) swap over @ + swap ! ; "
@@ -284,23 +280,23 @@ const char boot[] =
 " : state ( -- a ) 'sys 3 cells + ; "
 " : base ( -- a ) 'sys 4 cells + ; "
 " : 'here ( -- a ) 'sys 5 cells + ; "
-" : latest ( -- a ) 'sys 6 cells + ; "
+" : 'latest ( -- a ) 'sys 6 cells + ; "
 " : 'notfound ( -- a ) 'sys 7 cells + ; "
 
 // Dictionary 
 " : here ( -- a ) 'here @ ; "
+" : latest  'latest @ ; "
 " : allot ( n -- ) 'here +! ; "
-" : aligned ( a -- a ) cell 1 - dup >r + r> invert and ; "
-" : align   here aligned here - allot ; "
-" : , ( n --  ) here ! cell allot ; "
+" : aligned ( a -- a ) cell+ 1- cell negate and ; "
+" : align   here aligned 'here ! ; "
 " : c, ( ch -- ) here c! 1 allot ; "
 
 // Compilation State 
-" : [ 0 state ! ; immediate "
+" : [  0 state ! ; immediate "
 " : ] -1 state ! ; immediate "
 
 // Quoting Words 
-" : ' bl parse 2dup find dup >r -rot r> 0= 'notfound @ execute 2drop ; "
+//" : ' bl parse 2dup find dup >r -rot r> 0= 'notfound @ execute 2drop ; "
 " : ['] ' aliteral ; immediate "
 " : char bl parse drop c@ ; "
 " : [char] char aliteral ; immediate "
@@ -337,15 +333,24 @@ const char boot[] =
 // Counted Loops 
 " : for   postpone >r postpone begin ; immediate "
 " : next   postpone donext , ; immediate "
-" : do   postpone swap postpone >r postpone >r here ; immediate "
+" : dostart ( n n -- .. ) swap r> -rot >r >r >r ; "
+" : docheck ( n -- f .. ) r> r> rot + dup r@ < -rot >r >r ; "
+" : do   postpone dostart here 0 ; immediate "
+" : ?do   postpone dostart 0 aliteral postpone ahead here swap ; immediate "
 " : i   postpone r@ ; immediate "
 " : j   rp@ 3 cells - @ ; "
 " : unloop   postpone rdrop postpone rdrop ; immediate "
-" : +loop   postpone r> postpone + postpone r> "
-"           postpone 2dup postpone >r postpone >r "
-"           postpone < postpone 0= postpone until "
-"           postpone unloop ; immediate "
+" : +loop   dup if postpone then else drop then "
+"            postpone docheck postpone 0= postpone until "
+"            postpone unloop ; immediate "
 " : loop   1 aliteral postpone +loop ; immediate "
+
+// create and does> (taken from planckforth)
+" : nop ; "
+" : :noname 0 , latest , 0 , here dup 'latest ! 'docol , postpone ] ; "
+" : create   header here 4 cells + aliteral postpone nop postpone exit ; "
+" : (does>) ( n -- )  latest 3 cells + ! ; "
+" : does>  0 aliteral here cell - postpone (does>) postpone ; :noname swap ! ; immediate "
 
 // Constants and Variables 
 " : constant ( n \"name\" -- ) create , does> @ ; "
@@ -358,10 +363,8 @@ const char boot[] =
 
 // Exceptions 
 " variable handler "
-" : catch ( xt -- n ) "
-"   sp@ >r handler @ >r rp@ handler ! execute r> handler ! r> drop 0 ; "
-" : throw ( n -- ) "
-"   dup if handler @ rp! r> handler !  r> swap >r sp! drop r> else drop then ; "
+" : catch ( xt -- n )  sp@ >r handler @ >r rp@ handler ! execute r> handler ! r> drop 0 ; "
+" : throw ( n -- )     dup if handler @ rp! r> handler !  r> swap >r sp! drop r> else drop then ; "
 " ' throw 'notfound ! "
 
 // Values 
@@ -386,13 +389,17 @@ const char boot[] =
 " : hold ( c -- ) hld @ 1 - dup hld ! c! ; "
 " : # ( u -- u ) base @ extract hold ; "
 " : #s ( u -- 0 ) begin # dup while repeat ; "
+//" : #.n ( u n -- u )  0 do # loop ; "
 " : sign ( n -- ) 0< if 45 hold then ; "
 " : #> ( w -- b u ) drop hld @ pad over - ; "
 " : str ( n -- b u ) dup >r abs <# #s r> sign #> ; "
 " : hex ( -- ) 16 base ! ; "
 " : decimal ( -- ) 10 base ! ; "
 " : u. ( u -- ) <# #s #> type space ; "
-" : . ( w -- ) base @ 10 xor if u. exit then str type space ; "
+//" : u.n  ( u n -- )  <# 0 do # loop #> type space ; "
+" : #n ( u n -- u )  0 ?do # loop ; "
+" : u.0 ( u n -- )  <# 1- #n #s #> type ; "
+" : . ( w -- ) base @ 10 xor if u. else str type space then ; "
 " : ? ( a -- ) @ . ; "
 
 // Strings 
@@ -403,30 +410,27 @@ const char boot[] =
 "        else dup here swap >r >r $place r> r> then ; immediate "
 " : .\"   postpone s\" state @ if postpone type else type then ; immediate "
 " : z\"   postpone s\" state @ if postpone drop else drop then ; immediate "
-" : r\"   parse-quote state @ if swap aliteral aliteral then ; immediate "
 
 // Better Errors 
-" : notfound ( a n n -- ) "
-"    if cr .\" ERROR: \" type .\"  NOT FOUND!\" cr -1 throw then ; "
+" : notfound ( a n n -- )  if cr .\" ERROR: \" type .\"  NOT FOUND!\" cr -1 throw then ; "
 " ' notfound 'notfound ! "
 
 // Examine Dictionary 
 // TODO: crashes when decompiling:
-// - primitives
 // - words containing ." and s"
 // - loops/0branch
-// - empty words
+" : builtin? ( xt -- ) @ 'DOCOL <> ; "
 " : see. ( xt -- ) >name type space ; "
-" : see-one ( xt -- xt+1 ) "
-"    dup @ dup ['] DOLIT = if drop cell+ dup @ . else see. then cell+ ; "
+" : see-one ( xt -- xt+1 )  dup @ dup ['] DOLIT = if drop cell+ dup @ . else see. then cell+ ; "
 " : exit= ( xt -- ) ['] exit = ; "
-" : see-loop   >:body begin see-one dup @ exit= until ; "
-" : see   cr ['] : see.  ' dup see.  space see-loop drop  ['] ; see.  cr ; "
-" : words   latest @ begin dup see. >link dup 0= until drop cr ; "
+" : see-loop   >:body begin dup @ exit= invert while see-one repeat ; "
+" : see-immediate?   immediate? if .\"  immediate\" then ; "
+" : see-:   ['] : see.  dup see. space dup see-loop drop  ['] ; see.  see-immediate? ; "
+" : see   ' dup builtin? if dup see. ." is builtin" else see-: then cr ; "
+" : words   latest begin dup see. >link dup 0= until drop cr ; "
 
 // Examine Memory 
-" : dump ( a n -- ) "
-"    cr 0 do i 16 mod 0= if cr then dup i + c@ . loop drop cr ; "
+" : dump ( a n -- )  cr 0 do i 16 mod 0= if cr then dup i + c@ 2 u.0 space loop drop cr ; "
 
 // Input 
 " : accept ( a n -- n ) 0 swap begin 2dup < while "
@@ -439,7 +443,7 @@ const char boot[] =
 " : refill   tib-setup tib input-limit accept #tib ! 0 >in ! -1 ; "
 
 // REPL 
-" : prompt   .\"  ok\" cr ; "
+" : prompt   state @ if .\"  compiled\" else .\"  ok\" then cr ; "
 " : evaluate-buffer   begin >in @ #tib @ < while evaluate1 repeat ; "
 " : evaluate ( a n -- ) 'tib @ >r #tib @ >r >in @ >r "
 "                       #tib ! 'tib ! 0 >in ! evaluate-buffer "
@@ -448,9 +452,8 @@ const char boot[] =
 "           if 0 state ! sp0 sp! rp0 rp! .\" ERROR\" cr then "
 "           prompt refill drop again ; "
 " : ok   .\" uEForth\" cr prompt refill drop query ; "
-//" : bounds over + swap ; "
 " : .s .\" < \" depth . .\" > \" depth 0 = if exit then depth 0 do sp0 i 1 + cells + @ . loop cr ; "
-
+" : forget  ' dup >name drop 'here ! >link 'latest ! ; "
 
 " 200 ms "
 " ok "
