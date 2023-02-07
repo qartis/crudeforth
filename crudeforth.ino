6b1c8c725895b38c83e013bd5454aab3239b1587
Andrew Fuller <qartis@gmail.com>
2023-02-06 18:43:43 -0800

    move forth code to c++ raw string

--- crudeforth.ino
+++ crudeforth.ino
@@ -256,324 +256,224 @@ static void ueforth(void *here, const char *src, cell_t src_len) {
   OP_DOCOLON: ++rp; *rp = (cell_t) ip; ip = (cell_t *) (w + sizeof(cell_t)); NEXT;
 }
 
-const char boot[] =
-" HEADER : ' HEADER , -1 ALITERAL 'sys 3 cell * + ALITERAL ' ! , ' exit , "
-" HEADER ; ' exit ALITERAL ' , , 0 ALITERAL 'sys 3 cell * + ALITERAL ' ! , ' exit , IMMEDIATE "
+const char boot[] = R"(
+HEADER : ' HEADER , -1 ALITERAL 'sys 3 cell * + ALITERAL ' ! , ' exit ,
+HEADER ; ' exit ALITERAL ' , , 0 ALITERAL 'sys 3 cell * + ALITERAL ' ! , ' exit , IMMEDIATE
 
-" : bl 32 ; "
-" : nl 10 ; "
-" : (   41 parse drop drop ; immediate "
-" : \\   nl parse drop drop ; immediate "
-" : 2drop ( n n -- ) drop drop ; "
-" : 2dup ( a b -- a b a b ) over over ; "
-" : nip ( a b -- b ) swap drop ; "
-" : rdrop ( r: n n -- ) r> r> drop >r ; "
-" : */ ( n n n -- n ) */mod nip ; "
-" : /mod ( n n -- n n ) 1 swap */mod ; "
-" : / ( n n -- n ) /mod nip ; "
-" : mod ( n n -- n ) /mod drop ; "
-" : invert ( n -- ~n ) -1 xor ; "
-" : negate ( n -- -n ) invert 1 + ; "
-" : - ( n n -- n ) negate + ; "
-" : rot ( a b c -- c a b ) >r swap r> swap ; "
-" : -rot ( a b c -- b c a ) swap >r swap r> ; "
-" : < ( a b -- a<b ) - 0< ; "
-" : > ( a b -- a>b ) swap - 0< ; "
-" : = ( a b -- a!=b ) - 0= ; "
-" : <> ( a b -- a!=b ) = 0= ; "
-" : 1+ 1 + ; "
-" : 1- 1 - ; "
-" : +! ( n a -- ) swap over @ + swap ! ; "
-
-// Cells 
-" : cell+ ( n -- n ) cell + ; "
-" : cells ( n -- n ) cell * ; "
-" : cell/ ( n -- n ) cell / ; "
-
-// System Variables 
-" : 'tib ( -- a ) 'sys 0 cells + ; "
-" : #tib ( -- a ) 'sys 1 cells + ; "
-" : >in ( -- a ) 'sys 2 cells + ; "
-" : state ( -- a ) 'sys 3 cells + ; "
-" : base ( -- a ) 'sys 4 cells + ; "
-" : 'here ( -- a ) 'sys 5 cells + ; "
-" : 'latest ( -- a ) 'sys 6 cells + ; "
-" : 'notfound ( -- a ) 'sys 7 cells + ; "
-
-// Dictionary 
-" : here ( -- a ) 'here @ ; "
-" : latest  'latest @ ; "
-" : allot ( n -- ) 'here +! ; "
-" : aligned ( a -- a ) cell+ 1- cell negate and ; "
-" : align   here aligned 'here ! ; "
-" : c, ( ch -- ) here c! 1 allot ; "
-
-// Compilation State 
-" : [  0 state ! ; immediate "
-" : ] -1 state ! ; immediate "
-
-// Quoting Words 
-//" : ' bl parse 2dup find dup >r -rot r> 0= 'notfound @ execute 2drop ; "
-" : ['] ' aliteral ; immediate "
-" : char bl parse drop c@ ; "
-" : [char] char aliteral ; immediate "
-" : literal aliteral ; immediate "
-
-// Core Control Flow 
-" : begin   here ; immediate "
-" : again   ['] branch , , ; immediate "
-" : until   ['] 0branch , , ; immediate "
-" : ahead   ['] branch , here 0 , ; immediate "
-" : then   here swap ! ; immediate "
-" : if   ['] 0branch , here 0 , ; immediate "
-" : else   ['] branch , here 0 , swap here swap ! ; immediate "
-" : while   ['] 0branch , here 0 , swap ; immediate "
-" : repeat   ['] branch , , here swap ! ; immediate "
-" : aft   drop ['] branch , here 0 , here swap ; immediate "
-
-// Compound words requiring conditionals 
-" : min 2dup < if drop else nip then ; "
-" : max 2dup < if nip else drop then ; "
-" : abs ( n -- +n ) dup 0< if negate then ; "
-
-// Dictionary Format 
-" : >name ( xt -- a n ) 3 cells - dup @ swap over aligned - swap ; "
-" : >link ( xt -- a ) 2 cells - @ ; "
-" : >flags ( xt -- flags ) cell - ; "
-" : >body ( xt -- a ) 2 cells + ; "
-" : >:body ( xt -- a ) cell+ ; "
-
-// Postpone - done here so we have ['] and IF 
-" : immediate? ( xt -- f ) >flags @ 1 and 0= 0= ; "
-" : postpone ' dup immediate? if , else aliteral ['] , , then ; immediate "
-
-// Counted Loops 
-" : for   postpone >r postpone begin ; immediate "
-" : next   postpone donext , ; immediate "
-" : dostart ( n n -- .. ) swap r> -rot >r >r >r ; "
-" : docheck ( n -- f .. ) r> r> rot + dup r@ < -rot >r >r ; "
-" : do   postpone dostart here 0 ; immediate "
-" : ?do   postpone dostart 0 aliteral postpone ahead here swap ; immediate "
-" : i   postpone r@ ; immediate "
-" : j   rp@ 3 cells - @ ; "
-" : unloop   postpone rdrop postpone rdrop ; immediate "
-" : +loop   dup if postpone then else drop then "
-"            postpone docheck postpone 0= postpone until "
-"            postpone unloop ; immediate "
-" : loop   1 aliteral postpone +loop ; immediate "
-
-// create and does> (taken from planckforth)
-" : nop ; "
-" : :noname 0 , latest , 0 , here dup 'latest ! 'docol , postpone ] ; "
-" : create   header here 4 cells + aliteral postpone nop postpone exit ; "
-" : (does>) ( n -- )  latest 3 cells + ! ; "
-" : does>  0 aliteral here cell - postpone (does>) postpone ; :noname swap ! ; immediate "
-
-// Constants and Variables 
-" : constant ( n \"name\" -- ) create , does> @ ; "
-" : variable ( \"name\" -- ) create 0 , ; "
-
-// Stack Convience 
-" sp@ constant sp0 "
-" rp@ constant rp0 "
-" : depth ( -- n ) sp@ sp0 - cell/ ; "
-
-// Exceptions 
-" variable handler "
-" : catch ( xt -- n )  sp@ >r handler @ >r rp@ handler ! execute r> handler ! r> drop 0 ; "
-" : throw ( n -- )     dup if handler @ rp! r> handler !  r> swap >r sp! drop r> else drop then ; "
-" ' throw 'notfound ! "
-
-// Values 
-" : value ( n -- ) create , does> @ ; "
-" : to ( n -- ) state @ if postpone ['] postpone >body postpone ! "
-"                       else ' >body ! then ; immediate "
-
-// Deferred Words 
-" : defer ( \"name\" -- ) create 0 , does> @ dup 0= throw execute ; "
-" : is ( xt \"name -- ) postpone to ; immediate "
-
-" : emit ( n -- ) >r rp@ 1 type rdrop ; "
-" : space bl emit ; "
-" : cr nl emit ; "
-
-// Numeric Output 
-" variable hld "
-" : pad ( -- a ) here 80 + ; "
-" : digit ( u -- c ) 9 over < 7 and + 48 + ; "
-" : extract ( n base -- n c ) 0 swap um/mod swap digit ; "
-" : <# ( -- ) pad hld ! ; "
-" : hold ( c -- ) hld @ 1 - dup hld ! c! ; "
-" : # ( u -- u ) base @ extract hold ; "
-" : #s ( u -- 0 ) begin # dup while repeat ; "
-//" : #.n ( u n -- u )  0 do # loop ; "
-" : sign ( n -- ) 0< if 45 hold then ; "
-" : #> ( w -- b u ) drop hld @ pad over - ; "
-" : str ( n -- b u ) dup >r abs <# #s r> sign #> ; "
-" : hex ( -- ) 16 base ! ; "
-" : decimal ( -- ) 10 base ! ; "
-" : u. ( u -- ) <# #s #> type space ; "
-//" : u.n  ( u n -- )  <# 0 do # loop #> type space ; "
-" : #n ( u n -- u )  0 ?do # loop ; "
-" : u.0 ( u n -- )  <# 1- #n #s #> type ; "
-" : . ( w -- ) base @ 10 xor if u. else str type space then ; "
-" : ? ( a -- ) @ . ; "
-
-// Strings 
-" : parse-quote ( -- a n ) [char] \" parse ; "
-" : $place ( a n -- ) 0 do dup c@ c, 1+ loop drop 0 c, align ; "
-" : $@   r@ dup cell+ swap @ r> dup @ 1+ aligned + cell+ >r ; "
-" : s\"   parse-quote state @ if postpone $@ dup , $place "
-"        else dup here swap >r >r $place r> r> then ; immediate "
-" : .\"   postpone s\" state @ if postpone type else type then ; immediate "
-" : z\"   postpone s\" state @ if postpone drop else drop then ; immediate "
-
-// Better Errors 
-" : notfound ( a n n -- )  if cr .\" ERROR: \" type .\"  NOT FOUND!\" cr -1 throw then ; "
-" ' notfound 'notfound ! "
-
-// Examine Dictionary 
-// TODO: crashes when decompiling:
-// - words containing ." and s"
-// - loops/0branch
-" : builtin? ( xt -- ) @ 'DOCOL <> ; "
-" : see. ( xt -- ) >name type space ; "
-" : see-one ( xt -- xt+1 )  dup @ dup ['] DOLIT = if drop cell+ dup @ . else see. then cell+ ; "
-" : exit= ( xt -- ) ['] exit = ; "
-" : see-loop   >:body begin dup @ exit= invert while see-one repeat ; "
-" : see-immediate?   immediate? if .\"  immediate\" then ; "
-" : see-:   ['] : see.  dup see. space dup see-loop drop  ['] ; see.  see-immediate? ; "
-" : see   ' dup builtin? if dup see. .\" is builtin\" else see-: then cr ; "
-" : words   latest begin dup see. >link dup 0= until drop cr ; "
-
-// Examine Memory 
-" : dump ( a n -- )  cr 0 do i 16 mod 0= if cr then dup i + c@ 2 u.0 space loop drop cr ; "
-
-// Input 
-" : accept ( a n -- n ) 0 swap begin 2dup < while "
-"    key dup nl = if 2drop nip exit then "
-"    >r rot r> over c! 1+ -rot swap 1+ swap repeat drop nip ; "
-" 200 constant input-limit "
-" : tib ( -- a ) 'tib @ ; "
-" create input-buffer   input-limit allot "
-" : tib-setup   input-buffer 'tib ! ; "
-" : refill   tib-setup tib input-limit accept #tib ! 0 >in ! -1 ; "
-
-// REPL 
-" : prompt   state @ if .\"  compiled\" else .\"  ok\" then cr ; "
-" : evaluate-buffer   begin >in @ #tib @ < while evaluate1 repeat ; "
-" : evaluate ( a n -- ) 'tib @ >r #tib @ >r >in @ >r "
-"                       #tib ! 'tib ! 0 >in ! evaluate-buffer "
-"                       r> >in ! r> #tib ! r> 'tib ! ; "
-" : query   begin ['] evaluate-buffer catch "
-"           if 0 state ! sp0 sp! rp0 rp! .\" ERROR\" cr then "
-"           prompt refill drop again ; "
-" : ok   .\" uEForth\" cr prompt refill drop query ; "
-" : .s .\" < \" depth . .\" > \" depth 0 = if exit then depth 0 do sp0 i 1 + cells + @ . loop cr ; "
-" : forget  ' dup >name drop 'here ! >link 'latest ! ; "
-
-//" 4 constant led "
-//" 0 constant pwmchan "
-//" 12 constant 12bit "
-//" 19569 constant pwmfreq "
-//" pwmchan pwmfreq 12bit ledcSetup "
-//" led pwmchan ledcattachpin "
-//" : setled pwmchan swap ledcwrite ; "
-//" 2 setled "
-
-/*
-1 constant SOCK_STREAM
-2 constant SOCK_DGRAM
-3 constant SOCK_RAW
-
-2 constant AF_INET
-16 constant sizeof(sockaddr_in)
-1 constant SOL_SOCKET
-2 constant SO_REUSEADDR
-
-: bs, ( n -- ) dup 8 rshift c, c, ;
-: s, ( n -- ) dup c, 8 rshift c, ;
-: l, ( n -- ) dup s, 16 rshift s, ;
-: sockaddr   create 16 c, AF_INET c, 0 bs, 0 l, 0 l, 0 l, ;
-: ->port@ ( a -- n ) 2 + >r r@ c@ 8 lshift r> 1+ c@ + ;
-: ->port! ( n a --  ) 2 + >r dup 8 rshift r@ c! r> 1+ c! ;
-: ->addr@ ( a -- n ) 4 + ul@ ;
-: ->addr! ( n a --  ) 4 + l! ;
-: ->h_addr ( hostent -- n ) 2 cells + 8 + @ @ ul@ ;
-: ip# ( n -- n ) dup 255 and n. [char] . emit 8 rshift ;
-: ip. ( n -- ) ip# ip# ip# 255 and n. ;
+: bl 32 ;
+: nl 10 ;
+: (   41 parse drop drop ; immediate
+: 2drop ( n n -- ) drop drop ;
+: 2dup ( a b -- a b a b ) over over ;
+: nip ( a b -- b ) swap drop ;
+: rdrop ( r: n n -- ) r> r> drop >r ;
+: */ ( n n n -- n ) */mod nip ;
+: /mod ( n n -- n n ) 1 swap */mod ;
+: / ( n n -- n ) /mod nip ;
+: mod ( n n -- n ) /mod drop ;
+: invert ( n -- ~n ) -1 xor ;
+: negate ( n -- -n ) invert 1 + ;
+: - ( n n -- n ) negate + ;
+: rot ( a b c -- c a b ) >r swap r> swap ;
+: -rot ( a b c -- b c a ) swap >r swap r> ;
+: < ( a b -- a<b ) - 0< ;
+: > ( a b -- a>b ) swap - 0< ;
+: = ( a b -- a!=b ) - 0= ;
+: <> ( a b -- a!=b ) = 0= ;
+: 1+ 1 + ;
+: 1- 1 - ;
+: +! ( n a -- ) swap over @ + swap ! ;
 
 
+( \ Cells  )
+: cell+ ( n -- n ) cell + ;
+: cells ( n -- n ) cell * ;
+: cell/ ( n -- n ) cell / ;
 
--1 value sockfd   -1 value clientfd
-sockaddr telnet-port   sockaddr client   variable client-len
+( \ System Variables )
+: 'tib ( -- a ) 'sys 0 cells + ;
+: #tib ( -- a ) 'sys 1 cells + ;
+: >in ( -- a ) 'sys 2 cells + ;
+: state ( -- a ) 'sys 3 cells + ;
+: base ( -- a ) 'sys 4 cells + ;
+: 'here ( -- a ) 'sys 5 cells + ;
+: 'latest ( -- a ) 'sys 6 cells + ;
+: 'notfound ( -- a ) 'sys 7 cells + ;
 
-defer broker
+( \ Dictionary )
+: here ( -- a ) 'here @ ;
+: latest  'latest @ ;
+: allot ( n -- ) 'here +! ;
+: aligned ( a -- a ) cell+ 1- cell negate and ;
+: align   here aligned 'here ! ;
+: c, ( ch -- ) here c! 1 allot ;
 
-: telnet-emit ( ch -- ) >r rp@ 1 clientfd write-file rdrop if broker then ;
-: telnet-type ( a n -- ) for aft dup c@ telnet-emit 1+ then next drop ;
-: telnet-key ( -- n ) 0 >r rp@ 1 clientfd read-file swap 1 <> or if rdrop broker then r> ;
+( \ Compilation State )
+: [  0 state ! ; immediate
+: ] -1 state ! ; immediate
 
-: connection ( n -- )
-  dup 0< if drop exit then to clientfd
-  0 echo !
-  ['] telnet-key is key
-  ['] telnet-type is type quit ;
+( \ Quoting Words )
+( \ : ' bl parse 2dup find dup >r -rot r> 0= 'notfound @ execute 2drop ; )
+: ['] ' aliteral ; immediate
+: char bl parse drop c@ ;
+: [char] char aliteral ; immediate
+: literal aliteral ; immediate
 
-: wait-for-connection
-  begin
-    sockfd client client-len sockaccept
-    dup 0 >= if exit else drop then
-  again
-;
+( \ Core Control Flow  )
+: begin   here ; immediate
+: again   ['] branch , , ; immediate
+: until   ['] 0branch , , ; immediate
+: ahead   ['] branch , here 0 , ; immediate
+: then   here swap ! ; immediate
+: if   ['] 0branch , here 0 , ; immediate
+: else   ['] branch , here 0 , swap here swap ! ; immediate
+: while   ['] 0branch , here 0 , swap ; immediate
+: repeat   ['] branch , , here swap ! ; immediate
+: aft   drop ['] branch , here 0 , here swap ; immediate
 
-: broker-connection
-  rp0 rp! sp0 sp!
-  begin
-    ['] default-key is key   ['] default-type is type
-    -1 echo !
-    ." Listening on port " telnet-port ->port@ . cr
-    wait-for-connection
-    ." Connected: " dup . cr connection
-  again ;
-' broker-connection is broker
+( \ Compound words requiring conditionals  )
+: min 2dup < if drop else nip then ;
+: max 2dup < if nip else drop then ;
+: abs ( n -- +n ) dup 0< if negate then ;
 
-: server ( port -- )
-  telnet-port ->port!
-  AF_INET SOCK_STREAM 0 socket to sockfd
-  sockfd non-block throw
-  sockfd telnet-port sizeof(sockaddr_in) bind throw
-  sockfd 1 listen throw   broker ;
-*/
-" 2 constant ena "
-" 0 constant in1 "
-" 4 constant in2 "
-" 16 constant in3 "
-" 17 constant in4 "
-" 5 constant enb "
+( \ Dictionary Format  )
+: >name ( xt -- a n ) 3 cells - dup @ swap over aligned - swap ;
+: >link ( xt -- a ) 2 cells - @ ;
+: >flags ( xt -- flags ) cell - ;
+: >body ( xt -- a ) 2 cells + ;
+: >:body ( xt -- a ) cell+ ;
 
-" 0 constant pwmchan "
-" 30 constant pwmfreq "
-" 2 constant PINMODE_OUTPUT "
-" 2000 value rampduty "
-" 20 value ramptime "
-" 600 value driveduty "
+( \ Postpone - done here so we have ['] and IF  )
+: immediate? ( xt -- f ) >flags @ 1 and 0= 0= ;
+: postpone ' dup immediate? if , else aliteral ['] , , then ; immediate
 
-" pwmchan pwmfreq 12 ledcsetup drop "
-" ena pwmchan ledcattachpin "
+( \ Counted Loops  )
+: for   postpone >r postpone begin ; immediate
+: next   postpone donext , ; immediate
+: dostart ( n n -- .. ) swap r> -rot >r >r >r ;
+: docheck ( n -- f .. ) r> r> rot + dup r@ < -rot >r >r ;
+: do   postpone dostart here 0 ; immediate
+: ?do   postpone dostart 0 aliteral postpone ahead here swap ; immediate
+: i   postpone r@ ; immediate
+: j   rp@ 3 cells - @ ;
+: unloop   postpone rdrop postpone rdrop ; immediate
+: +loop   dup if postpone then else drop then
+           postpone docheck postpone 0= postpone until
+           postpone unloop ; immediate
+: loop   1 aliteral postpone +loop ; immediate
 
-" in1 PINMODE_OUTPUT pinmode "
-" in2 PINMODE_OUTPUT pinmode "
+( \ create and does> (taken from planckforth) )
+: nop ;
+: :noname 0 , latest , 0 , here dup 'latest ! 'docol , postpone ] ;
+: create   header here 4 cells + aliteral postpone nop postpone exit ;
+: (does>) ( n -- )  latest 3 cells + ! ;
+: does>  0 aliteral here cell - postpone (does>) postpone ; :noname swap ! ; immediate
 
-" : off ( -- ) pwmchan 0 ledcwrite ; "
-" : pwmduty  ( n -- ) pwmchan swap ledcwrite ; "
-" : go ( -- )  rampduty pwmduty   ramptime ms   driveduty pwmduty ; "
-" : forw  ( -- ) in1 1 digitalWrite in2 0 digitalWrite ; "
-" : back  ( -- ) in1 0 digitalWrite in2 1 digitalWrite ; "
+( \ Constants and Variables  )
+: constant ( n "name" -- ) create , does> @ ;
+: variable ( "name" -- ) create 0 , ;
 
-" 200 ms "
-" ok "
-;
+( \ Stack Convience  )
+sp@ constant sp0
+rp@ constant rp0
+: depth ( -- n ) sp@ sp0 - cell/ ;
+
+( \ Exceptions  )
+variable handler
+: catch ( xt -- n )  sp@ >r handler @ >r rp@ handler ! execute r> handler ! r> drop 0 ;
+: throw ( n -- )     dup if handler @ rp! r> handler !  r> swap >r sp! drop r> else drop then ;
+' throw 'notfound !
+
+( \ Values  )
+: value ( n -- ) create , does> @ ;
+: to ( n -- ) state @ if postpone ['] postpone >body postpone !
+                      else ' >body ! then ; immediate
+
+( \ Deferred Words  )
+: defer ( "name" -- ) create 0 , does> @ dup 0= throw execute ;
+: is ( xt "name" -- ) postpone to ; immediate
+
+: emit ( n -- ) >r rp@ 1 type rdrop ;
+: space bl emit ;
+: cr nl emit ;
+
+( \ Numeric Output  )
+variable hld
+
+: pad ( -- a ) here 80 + ;
+: digit ( u -- c ) 9 over < 7 and + 48 + ;
+: extract ( n base -- n c ) 0 swap um/mod swap digit ;
+: <# ( -- ) pad hld ! ;
+: hold ( c -- ) hld @ 1 - dup hld ! c! ;
+: # ( u -- u ) base @ extract hold ;
+: #s ( u -- 0 ) begin # dup while repeat ;
+: sign ( n -- ) 0< if 45 hold then ;
+: #> ( w -- b u ) drop hld @ pad over - ;
+: str ( n -- b u ) dup >r abs <# #s r> sign #> ;
+: hex ( -- ) 16 base ! ;
+: decimal ( -- ) 10 base ! ;
+: u. ( u -- ) <# #s #> type space ;
+: #n ( u n -- u )  0 ?do # loop ;
+: u.0 ( u n -- )  <# 1- #n #s #> type ;
+: . ( w -- ) base @ 10 xor if u. else str type space then ;
+: ? ( a -- ) @ . ;
+
+( \ Strings  )
+: parse-quote ( -- a n ) [char] " parse ;
+: $place ( a n -- ) 0 do dup c@ c, 1+ loop drop 0 c, align ;
+: $@   r@ dup cell+ swap @ r> dup @ 1+ aligned + cell+ >r ;
+: s"   parse-quote state @ if postpone $@ dup , $place
+       else dup here swap >r >r $place r> r> then ; immediate
+: ."   postpone s" state @ if postpone type else type then ; immediate
+: z"   postpone s" state @ if postpone drop else drop then ; immediate
+
+( \ Better Errors  )
+: notfound ( a n n -- )  if cr ." ERROR: " type ."  NOT FOUND!" cr -1 throw then ;
+' notfound 'notfound !
+
+( \ Examine Dictionary  )
+( \ TODO: crashes when decompiling: )
+( \ - words containing ." and s" )
+( \ - loops/0branch )
+: builtin? ( xt -- ) @ 'DOCOL <> ;
+: see. ( xt -- ) >name type space ;
+: see-one ( xt -- xt+1 )  dup @ dup ['] DOLIT = if drop cell+ dup @ . else see. then cell+ ;
+: exit= ( xt -- ) ['] exit = ;
+: see-loop   >:body begin dup @ exit= invert while see-one repeat ;
+: see-immediate?   immediate? if ."  immediate" then ;
+: see-:   ['] : see.  dup see. space dup see-loop drop  ['] ; see.  see-immediate? ;
+: see   ' dup builtin? if dup see. ." is builtin" else see-: then cr ;
+: words   latest begin dup see. >link dup 0= until drop cr ;
+
+( \ Examine Memory  )
+: dump ( a n -- )  cr 0 do i 16 mod 0= if cr then dup i + c@ 2 u.0 space loop drop cr ;
+
+( \ Input  )
+: accept ( a n -- n ) 0 swap begin 2dup < while
+   key dup nl = if 2drop nip exit then
+   >r rot r> over c! 1+ -rot swap 1+ swap repeat drop nip ;
+200 constant input-limit
+: tib ( -- a ) 'tib @ ;
+create input-buffer   input-limit allot
+: tib-setup   input-buffer 'tib ! ;
+: refill   tib-setup tib input-limit accept #tib ! 0 >in ! -1 ;
+
+( \ REPL  )
+: prompt   state @ if ."  compiled" else ."  ok" then cr ;
+: evaluate-buffer   begin >in @ #tib @ < while evaluate1 repeat ;
+: evaluate ( a n -- ) 'tib @ >r #tib @ >r >in @ >r
+                      #tib ! 'tib ! 0 >in ! evaluate-buffer
+                      r> >in ! r> #tib ! r> 'tib ! ;
+: query   begin ['] evaluate-buffer catch
+          if 0 state ! sp0 sp! rp0 rp! ." ERROR" cr then
+          prompt refill drop again ;
+: ok   ." uEForth" cr prompt refill drop query ;
+: .s ." < " depth . ." > " depth 0 = if exit then depth 0 do sp0 i 1 + cells + @ . loop cr ;
+: forget  ' dup >name drop 'here ! >link 'latest ! ;
+
+200 ms
+ok
+)";
 
 
 void setup() {
