b7eb73d2dc0a5d108f851933c46f1b8233ca0166
Andrew Fuller <qartis@gmail.com>
2023-02-20 22:07:52 -0800

    add +icanon flag to rlwrap, add -m for rlwrap multiline editing with ctrl-^

--- tools/serial-monitor.sh
+++ tools/serial-monitor.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 PORT=$1
-TERMIOS=b115200,echo=0,icrnl=0
+TERMIOS=b115200,echo=0,icrnl=0,icanon=0
 
 touch /tmp/done_uploading
 
-while rlwrap socat FILE:$PORT,$TERMIOS STDOUT; [ $? -ne 130 ]; do
+while rlwrap -m socat FILE:$PORT,$TERMIOS STDOUT; [ $? -ne 130 ]; do
 	echo waiting for upload finish
 	inotifywait -qq /tmp/done_uploading
 done
