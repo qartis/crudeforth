73b28ac4ae6392fefb2d729711fd7754657e930b
Andrew Fuller <qartis@gmail.com>
2023-03-12 15:36:20 -0700

    More explicit termios, disable onlcr

--- tools/serial-monitor.sh
+++ tools/serial-monitor.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 PORT=$1
-TERMIOS=b115200,echo=0,icrnl=0,icanon=0
+TERMIOS=b115200,echo=0,icrnl=0,icanon=0,isig=0,onlcr=0 #,opost=0,ixon=0,iexten=0,echoe=0,echok=0,min=0,echoctl=0,echoke=0
 
 touch /tmp/done_uploading
 
-while rlwrap -m socat FILE:$PORT,$TERMIOS STDOUT; [ $? -ne 130 ]; do
+while rlwrap -a -m socat FILE:$PORT,$TERMIOS STDOUT; [ $? -ne 130 ]; do
 	echo waiting for upload finish
 	inotifywait -qq /tmp/done_uploading
 done
